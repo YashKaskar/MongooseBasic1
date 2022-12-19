60539017ab27468862d4dda8cccaf679ed764f96
Yash Kaskar <yash.kaskar99@gmail.com>
2022-12-19 14:58:33 +0530

    connecting with mongoose

---
+++ product.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true)
+
+mongoose.connect('mongodb://localhost:27017/movieApps')
+.then(() =>{
+    console.log(" Connection Open !!!!")
+})
+.catch(err => {
+    console.log("Ohh No Error !!!")
+    console.log(err)
+})
