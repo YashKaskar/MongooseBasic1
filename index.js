4c7a941588edfc666dd66392fd08398a5d33eaaf
Yash Kaskar <yash.kaskar99@gmail.com>
2022-12-19 10:06:08 +0530

    new movies

--- index.js
+++ index.js
@@ -21,5 +21,11 @@ const Movie = mongoose.model('Movie', movieSchema);
 const movieName = new Movie({ title: 'Your Name', year: 2016, score: 8.4, rating: 'R' });
 
 //Inserting multiple movies
-
+Movie.insertMany([
+    { title: 'Amelie', year: 2001 , score: 8.3, rating: 'R' },
+    { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
+    { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
+    { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
+    { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'pG-13' }
+])
 
